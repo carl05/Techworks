9cbbd9b2e92280763f6d982a3c712ad78b4c3426
Igor Silva <igortkd2006@gmail.com>
2023-06-06 00:07:38 +0100

    story 1 layout: rename adapter

--- app/src/main/java/com/techworkscc/MainActivity.kt
+++ app/src/main/java/com/techworkscc/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.techworkscc.presentation.HeadLinesAdapter
+import com.techworkscc.presentation.NewsAdapter
 import com.techworkscc.presentation.NewsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         val recyclerView: RecyclerView = findViewById(R.id.rv_news)
         recyclerView.layoutManager = LinearLayoutManager(this)
         newsViewModel.newsLivewData.observe(this) {
-            val newsAdapter = HeadLinesAdapter(it.listNewsVO)
+            val newsAdapter = NewsAdapter(it.listNewsVO)
             recyclerView.adapter = newsAdapter
         }
         newsViewModel.loadNews()
--- app/src/main/java/com/techworkscc/presentation/NewsAdapter.kt
+++ app/src/main/java/com/techworkscc/presentation/NewsAdapter.kt
@@ -10,9 +10,9 @@ import com.bumptech.glide.Glide
 import com.techworkscc.R
 import com.techworkscc.domain.NewsVO
 
-class HeadLinesAdapter( private var data: List<NewsVO> = emptyList()): RecyclerView.Adapter<HeadLinesAdapter.ViewHolder>() {
+class NewsAdapter(private var data: List<NewsVO> = emptyList()): RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
 
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        class NewsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             val tvTitle: TextView
             val ivCover: ImageView
 
@@ -22,18 +22,18 @@ class HeadLinesAdapter( private var data: List<NewsVO> = emptyList()): RecyclerV
             }
         }
 
-        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): NewsViewHolder {
             // Create a new view, which defines the UI of the list item
             val view = LayoutInflater.from(viewGroup.context)
                 .inflate(R.layout.news_head_item, viewGroup, false)
 
-            return ViewHolder(view)
+            return NewsViewHolder(view)
         }
 
-        override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-            viewHolder.tvTitle.text = data[position].title
-            Glide.with(viewHolder.ivCover.context)
-                .load(data[position].imageCover).into(viewHolder.ivCover)
+        override fun onBindViewHolder(newsViewHolder: NewsViewHolder, position: Int) {
+            newsViewHolder.tvTitle.text = data[position].title
+            Glide.with(newsViewHolder.ivCover.context)
+                .load(data[position].imageCover).into(newsViewHolder.ivCover)
 
         }
         override fun getItemCount() = data.size
